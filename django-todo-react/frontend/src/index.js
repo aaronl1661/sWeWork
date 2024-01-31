468b276e47dbbe66b1abaa14f3055679a5c98e9f
Abhiram Kagitha <abhi@wireless-10-104-217-224.umd.edu>
2024-01-31 18:51:16 -0500

    worked on bootstrap

--- django-todo-react/frontend/src/index.js
+++ django-todo-react/frontend/src/index.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.css';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import "./index.html";
+import "/Users/abhi/SweWork/sWeWork/django-todo-react/frontend/src/css/styles.css";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.css";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
