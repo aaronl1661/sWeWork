a5e1558bacc43dadd3afae06346748cd3058b6e6
Aaron Lin <alin1238@umd.edu>
2024-02-24 02:28:46 -0500

    migrate to react

--- django-todo-react/backend/utils/generateSprint.py
+++
@@ -1,9 +0,0 @@
-from openai import OpenAI
-
-client = OpenAI()
-
-topic = "web scraper"
-completion = client.chat.completions.create(model="gpt-3.5-turbo", 
-messages=[{"role": "user", "content": "Write code for a " + topic + " with one bug and one feature that needs to be implemented"}])
-
-print(completion)
